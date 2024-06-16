83efdbe8f81c9365be6778d7d1ac351115b2a135
Tariqabed <60286385+Tariqabed@users.noreply.github.com>
2024-06-16 20:39:28 +0200

    Update sodku_better_version_.py

--- sodku_better_version_.py
+++ sodku_better_version_.py
@@ -1,144 +1,154 @@
 import numpy as np
-import heapq
+import threading
 
 class SolveAC:
+    def __init__(self):
+        self.related_cells = {}
+        self.dim = 0
+        self.grid_dim = 0
+        self.domains = {}
+
     def grid_insert(self, sudoku, dim):
+        self.initialize_grid(sudoku, dim)
+        self.assign_values()
+        self.calculate_relations()
+        self.ac3()  # Apply AC-3 algorithm
+
+    def initialize_grid(self, sudoku, dim):
         self.dim = dim
+        self.grid_dim = int(np.sqrt(dim))
         self.sudoku = np.array(sudoku)
-        self.variables = [(r, c) for r in range(self.dim) for c in range(self.dim)]
-        self.domains = {v: set(range(1,self.dim)) for v in self.variables}
-        self.assigned = {}
-        self.sudoku_reduction()
-        
+
+    def assign_values(self):
+        self.domains = {(r, c): set(range(1, self.dim + 1)) for r in range(self.dim) for c in range(self.dim)}
+
+        for r in range(self.dim):
+            for c in range(self.dim):
+                if self.sudoku[r, c] != 0:
+                    value = self.sudoku[r, c]
+                    self.domains[(r, c)] = {value}
+
+    def calculate_relations(self):
+        for r in range(self.dim):
+            for c in range(self.dim):
+                coords = (r, c)
+                self.related_cells[coords] = self.get_related_cells(coords)
+
+    def get_related_cells(self, coords):
+        related = set()
+
+        # Add cells in the same row and column
         for i in range(self.dim):
-            for j in range(self.dim):
-                if self.sudoku[i, j] != 0:
-                    self.domains[(i, j)] = {self.sudoku[i, j]}
-                    self.assigned[(i, j)] = self.sudoku[i, j]
+            related.add((i, coords[1]))  # Same row
+            related.add((coords[0], i))  # Same column
 
-        self.sudoku_reduction()
+        # Add cells in the same subgrid
+        row_index = (coords[0] // self.grid_dim) * self.grid_dim
+        col_index = (coords[1] // self.grid_dim) * self.grid_dim
+        for row_iter in range(self.grid_dim):
+            for col_iter in range(self.grid_dim):
+                related.add((row_iter + row_index, col_iter + col_index))
 
-        # Solve the puzzle using backtracking algorithm
-        self.priority_queue()
-        self.backtrack_algorithm()
+        related.remove(coords)  # Remove the original cell
+        return related
 
     def is_valid(self, row, col, value):
-        # Check if the value is valid in the row and column
-        for i in range(self.dim):
-            if (i, col) in self.assigned and self.assigned[(i, col)] == value:
-                return False
-            if (row, i) in self.assigned and self.assigned[(row, i)] == value:
-                return False
-        
-        # Check if the value is valid in the 3x3 subgrid
-        box_start_row, box_start_col = row - row % int(np.sqrt(self.dim)), col - col % int(np.sqrt(self.dim))
-        return not self.used_in_matrix(box_start_row, box_start_col, value)
+        relation = self.related_cells[(row, col)]
+        for index in relation:
+            r, c = index[0], index[1]
+            if len(self.domains[(r, c)]) == 1:
+                if list(self.domains[(r, c)])[0] == value:
+                    return False
+        return True
 
-    def used_in_matrix(self, start_row, start_col, num):
-        for i in range(int(np.sqrt(self.dim))):
-            for j in range(int(np.sqrt(self.dim))):
-                if self.sudoku[start_row + i, start_col + j] == num:
-                    return True
+    def enter_element(self, row, col, value):
+        if self.is_valid(row, col, value):
+            self.domains[(row, col)] = {value}
+            return self.ac3()
         return False
 
-    def sudoku_reduction(self):
-        for (row, col), value in self.assigned.items():
-            # Reduce the domain for the row
-            for j in range(self.dim):
-                if j != col and value in self.domains[(row, j)]:
-                    self.domains[(row, j)].remove(value)
-        
-            # Reduce the domain for the column
-            for i in range(self.dim):
-                if i != row and value in self.domains[(i, col)]:
-                    self.domains[(i, col)].remove(value)
-        
-            # Reduce the domain for the subgrid
-            box_start_row, box_start_col = row - row % int(np.sqrt(self.dim)), col - col % int(np.sqrt(self.dim))
-            for i in range(int(np.sqrt(self.dim))):
-                for j in range(int(np.sqrt(self.dim))):
-                    cell = (box_start_row + i, box_start_col + j)
-                    if cell != (row, col) and value in self.domains[cell]:
-                        self.domains[cell].remove(value)
+    def ac3(self):
+        queue = [(xi, xj) for xi in self.domains for xj in self.related_cells[xi]]
+        while queue:
+            (xi, xj) = queue.pop(0)
+            if self.revise_arc(xi, xj):
+                if len(self.domains[xi]) == 0:
+                    return False
+                for xk in self.related_cells[xi]:
+                    if xk != xj:
+                        queue.append((xk, xi))
+        return True
 
-    def insert_num(self, row, col, num):
-        # Check if the cell is already assigned
-        if (row, col) in self.assigned:
-            return False
-        
-        # Check if the number is in the domain of the cell
-        if num not in self.domains[(row, col)]:
-            return False
-        
-        # Check if the number is valid in the current context
-        if self.is_valid(row, col, num):
-            # Remove the number from the domains of related variables
-            for i in range(self.dim):
+    def revise_arc(self, xi, xj):
+        revised = False
+        for x in set(self.domains[xi]):
+            if not any(x != y for y in self.domains[xj]):
+                self.domains[xi].remove(x)
+                revised = True
+        return revised
 
-                if num in self.domains[(row, i)]:
-                    self.domains[(row, i)].remove(num)
-                if num in self.domains[(i, col)]:
-                    self.domains[(i, col)].remove(num)
+    def unfinished_cells(self):
+        index = self.binary_search_first_greater(list(self.domains.values()), 1)
+        if index != -1 : return list(self.domains.items())[index]
+        return None
+    @staticmethod
+    def binary_search_first_greater(arr, target):
+        left, right = 0, len(arr) - 1
+        result = -1  # Default value if no element is greater than target
+
+        while left <= right:
+            mid = left + (right - left) // 2
             
-            # Remove the number from the domain of the subgrid variables
-            box_start_row, box_start_col = row - row % int(np.sqrt(self.dim)), col - col % int(np.sqrt(self.dim))
-            for i in range(int(np.sqrt(self.dim))):
-                for j in range(int(np.sqrt(self.dim))):
-                    if num in self.domains[(box_start_row + i, box_start_col + j)]:
-                        self.domains[(box_start_row + i, box_start_col + j)].remove(num)
-            
-            # Assign the number
-            self.sudoku[row, col] = num
-            self.assigned[(row, col)] = num
-            self.domains[(row, col)] = {num}
+            if len(arr[mid]) > target:
+                result = mid  # Update result to the current index
+                right = mid - 1  # Search the left half
+            else:
+                left = mid + 1  # Search the right half
+
+        return result
+
+    def remove_element(self, row, col, value):
+        if (row, col) in self.domains and value in self.domains[(row, col)]:
+            self.domains[(row, col)].remove(value)
+
+    def backtrack(self):
+        if self.unfinished_cells() is None:
             return True
+
+        cell = self.unfinished_cells()
+
+        (row, col), possible_values = cell[0], cell[1]
+
+        domains_backup = {k: v.copy() for k, v in self.domains.items()}
+
+        for value in possible_values:
+            if self.enter_element(row, col, value):
+                if self.backtrack():
+                    return True
+
+            self.domains = {k: v.copy() for k, v in domains_backup.items()}
+            self.remove_element(row, col, value)
+
         return False
 
-    def priority_queue(self):
-        self.priority = []
-        for key, value in self.domains.items():
-            if len(value) != 1:
-                heapq.heappush(self.priority, (len(value), key, value))
-        
-    def backtrack_algorithm(self):
-        if len(self.priority) == 0 :
-            self.priority_queue()
-        try:
-            # Get the next element with the fewest possibilities
-            
-            current_element = heapq.heappop(self.priority)
-            row, col = current_element[1]
 
-            # Try each possible value for the current element
-            for value in current_element[2]:
-                # Attempt to insert the value
-                if self.insert_num(row, col, value):
-                    # If successful, move to the next element
-                    if self.backtrack_algorithm():
-                        return True
-                    # If the current branch leads to a dead end, backtrack
-                    self.remove_num(row, col)
 
-            # If no valid number can be inserted, backtrack
-            return False
-        except:
-            return False
+# Example Sudoku grid
+sudoku = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0]
+]
 
-            return False
+solver = SolveAC()
+solver.grid_insert(sudoku, 9)
 
-sudoku_puzzle = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
- 
-# Create the solver instance and initialize the grid
-test = SolveAC()
-test.grid_insert(sudoku_puzzle, 9)
-test.backtrack_algorithm()
-print(test.domains)
 
+solver.backtrack()
+print(solver.domains)
